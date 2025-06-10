845c563f36956d688d953b88d657b4f45b66e80f
shahirahzaki <shrhmdzk@gmail.com>
2025-06-10 22:45:59 +0800

    refactor: enhance favourite icon animation in MealDetailsScreen with AnimatedSwitcher

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -36,7 +36,19 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: Icon(isFavourite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  turns: Tween<double>(begin: 0.5, end: 1).animate(animation),
+                  child: child,
+                );
+              },
+              child: Icon(
+                isFavourite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavourite),
+              ),
+            ),
           ),
         ],
       ),
